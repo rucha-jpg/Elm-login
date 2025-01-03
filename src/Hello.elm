576b028b9673aa30b865436bf9da84fbd586c4ec
rucha-jpg <roochamahale@gmail.com>
2025-01-03 15:41:41 +0530

    ul li

--- src/Hello.elm
+++ src/Hello.elm
@@ -4,5 +4,12 @@ import Html as H
 
 
 main =
-    H.div [] [
-  H.text "Hello, World!" ]
+    H.div []
+    [
+        H.ul []
+        [
+            H.li [] [H.text "Pune"],
+            H.li [] [H.text "mumbai"]
+        ],
+        H.text "Hello, World!" 
+    ]
