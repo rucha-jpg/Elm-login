9661a9632b23d7b18654f21f3195467d47a362f0
rucha-jpg <roochamahale@gmail.com>
2025-01-03 15:17:40 +0530

    elm project

---
+++ src/Hello.elm
@@ -0,0 +1,8 @@
+module Hello exposing(..)
+
+import Html as H
+
+
+main =
+    H.div [] [
+  H.text "Hello, World!" ]
--- src/main.js
+++ src/main.js
@@ -1,24 +1,8 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+//import './style.css'
+import { Elm } from './Hello.elm'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+// Mount "Hello" Browser.{element,document} on #root
+Elm.Hello.init({
+  node: document.getElementById('app'),
+  flags: "Initial Message"
+})
